ed6690d06b168b0f3ed8bd2509d89f567668790b
Mike Burke <mikeburke.214@gmail.com>
2020-10-25 16:36:32 +0000

    Combined EndpointInterface & RestrictedEndpointInterface

--- src/Endpoint/EndpointInterface.php
+++ src/Endpoint/EndpointInterface.php
@@ -2,8 +2,17 @@
 
 namespace Electra\Web\Endpoint;
 
+use Electra\Core\Event\AbstractPayload;
+
 interface EndpointInterface
 {
   /** @return bool */
   public function requiresAuth(): bool;
+
+  /**
+   * @param AbstractPayload $payload
+   *
+   * @return bool
+   */
+  public function hasAccess($payload): bool;
 }
