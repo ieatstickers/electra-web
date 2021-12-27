6a64652e6439fccaa9fc7a88af439674cd3a14e9
mikeburke <mikeburke.214@gmail.com>
2021-12-27 16:04:56 +0000

    Added Request getOriginDomain method

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request as IlluminateRequest;
 
 class Request extends IlluminateRequest
 {
-
+  /** @return string */
+  public function getOriginDomain(): string
+  {
+    $origin = $this->headers->get('origin') ?? $_SERVER['HTTP_ORIGIN'];
+    return parse_url($origin, PHP_URL_HOST);
+  }
 }
